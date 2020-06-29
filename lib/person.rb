5ae90959ae406d078f1a20149304b8710de88a6c
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-29 18:04:03 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+ end
+
+ def name
+  @Will
+ end
+
+def walk
+   puts "The Person is walking"
+ end
+
+adele_goldberg = Person.new
+puts adele_goldberg
+
+alan_kay = Person.new
+puts alan_kay
